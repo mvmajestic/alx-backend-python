e8120fd262c31478be056aee05a30f8b1a6168ef
Vuyisanani Mkutyukelwa <vuyisanani@ymail.com>
2024-06-06 18:34:31 +0000

    Corrections Task 6: Complex types - mixed list

--- 0x00-python_variable_annotations/6-main.py
+++ 0x00-python_variable_annotations/6-main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-"""Contains a function that converts a Python variable to a KV pair."""
-from typing import Union, Tuple
 
+sum_mixed_list = __import__('6-sum_mixed_list').sum_mixed_list
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """Converts a Python variable to a KV pair."""
-    return k, v ** 2
+print(sum_mixed_list.__annotations__)
+mixed = [5, 4, 3.14, 666, 0.99]
+ans = sum_mixed_list(mixed)
+print(ans == sum(mixed))
+print("sum_mixed_list(mixed) returns {} which is a {}".format(ans, type(ans)))
