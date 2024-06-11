607f9d85169e942df4e065e0f60219d2f1880008
Vuyisanani Mkutyukelwa <vuyisanani@ymail.com>
2024-06-11 17:35:40 +0000

    Task 2: Run time for four parallel comprehensions

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Python module to measure the execution time"""
+import time
+import asyncio
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    measure_runtime - A function that executes async_com 4 times
+    Arguments:
+        nothing
+    Returns:
+        total exection time required to complete the task
+    """
+    t_start = time.perf_counter()
+    task = [async_comprehension() for i in range(4)]
+    await asyncio.gather(*task)
+    t_end = time.perf_counter()
+    return (t_end - t_start)
